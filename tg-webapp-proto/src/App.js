c77a730d5abdf07e932a9db9fa1240b4c64c2f60
porta_simmons <rpistsovfrm@yandex.ru>
2024-03-23 17:02:16 +0500

    Создано react-приложение

---
+++ tg-webapp-proto/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Poizon Shop
+        </p>
+      </header>
+      <body className="App-body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Under construction.</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
