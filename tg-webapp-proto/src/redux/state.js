3c3b79417aca17d0e416ded745aed009ff08dbca
Roman <rpistsovfrm@gmail.com>
2024-05-08 01:09:12 +0500

    Добавлен state-файл

---
+++ tg-webapp-proto/src/redux/state.js
@@ -0,0 +1,14 @@
+let state = {
+    posts: [
+      {message:'Enjoying sun beams', likes:'6', image:'https://storage-api.petstory.ru/resize/1000x1000x80/ec/d8/0b/ecd80bf6e6e049a79dc0557af65163f8.jpeg'},
+      {message:'Me and the boys', likes:'6', image:'https://opis-cdn.tinkoffjournal.ru/mercury/image1_samoed.rnfe0cvg7vbj.jpg'},
+      {message:'On my way to vacation. See ya!', likes:'6', image:'https://external-preview.redd.it/g-V-aWS125HJp5k35Z1GD5dxo858DPyew9tIylC6TrE.jpg?auto=webp&s=7e450cbf732023ef916bcf0bfa0d99077c6ffa8f'}
+    ],
+    profile: {
+      alt:"good boy", src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTl-dk_1ClaPOrcLAUjjNGHt_D2uMcpnJarvXob0phHw&s",
+      name:'Charlie, the Dogg', bio:'10 y.o. professional good boy'
+  }
+}
+
+
+export default state;
